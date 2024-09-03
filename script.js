58cbe3d0ddf13047bcaaa24cef20d28f108f4e3c
Jony <125291028+L19ER@users.noreply.github.com>
2024-09-03 14:19:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+function hideTabs() {
+  tabsItems.forEach((item) => item.classList.add("hide"));
+  tabsBtns.forEach((item) => item.classList.remove("active"));
+}
+hideTabs();
+
+function showTab(index) {
+  tabsItems[index].classList.remove("hide");
+  tabsBtns[index].classList.add("active");
+}
+showTab(0);
+
+tabsBtns.forEach((btn, index) =>
+  btn.addEventListener("click", () => {
+    hideTabs();
+    showTab(index);
+  })
+);
+
+const anchors = document.querySelectorAll(".header__nav a");
+anchors.forEach((anc) => {
+  anc.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    const elem = document.querySelector(id);
+
+    window.scroll({
+      top: elem.offsetTop - 74,
+      behavior: "smooth",
+    });
+  });
+});
